c0c8d97a3a956e955864150555ee337d07761b19
qasemB <mahdicmptr@gmail.com>
2022-09-09 12:44:46 +0430

    v-0

---
+++ detectCss.js
@@ -0,0 +1,17 @@
+let cssCodeTexts = ""
+const docStylsheet = document.styleSheets
+for (const cssFile in docStylsheet) {
+  if (typeof(docStylsheet[cssFile].href) == "string" && docStylsheet[cssFile].href.includes('util.css')) {
+    const cssCodes = docStylsheet[cssFile].cssRules
+
+    for (const css in cssCodes) {
+      
+      if (cssCodes[css].selectorText && document.querySelector('body').innerHTML.includes(cssCodes[css].selectorText.split(".")[1] || "zzzzzzzzzz")) {
+        cssCodeTexts = cssCodeTexts + cssCodes[css].cssText
+      }
+    }
+  }
+}
+var download = document.getElementById('download');
+download.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(cssCodeTexts));
+download.setAttribute('download', 'filename.css');
